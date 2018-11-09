bfef59999eb1814250994cc8269d0e240fb699d5
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-11-08 18:57:44 -0600

    Updates

---
+++ precache-manifest.78a22bef7e2d512425d06a794f6d520f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brendamorales/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99b7cf035f143ff26c0d",
+    "url": "/brendamorales/static/js/runtime~main.99b7cf03.js"
+  },
+  {
+    "revision": "57d0aa7d178bbaed2877",
+    "url": "/brendamorales/static/js/main.57d0aa7d.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/brendamorales/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "57d0aa7d178bbaed2877",
+    "url": "/brendamorales/static/css/main.b76f8069.chunk.css"
+  },
+  {
+    "revision": "e4a0dae53e1a5d2f61bc7ec1e590370f",
+    "url": "/brendamorales/index.html"
+  }
+];
